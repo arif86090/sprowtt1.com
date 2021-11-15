de6cb59d498e2d0bdaee40347d8fbced2215e072
arif86090 <73172108+arif86090@users.noreply.github.com>
2021-11-15 13:14:59 +0600

    Add files via upload

---
+++ launch.js
@@ -0,0 +1,152 @@
+var popup11=document.querySelector('.popup11');
+var btn11=document.querySelector('.btn11');
+var closs11=document.querySelector('.closs11');
+
+var popup12=document.querySelector('.popup12');
+var btn12=document.querySelector('.btn12');
+var closs12=document.querySelector('.closs12');
+
+var popup13=document.querySelector('.popup13');
+var btn13=document.querySelector('.btn13');
+var closs13=document.querySelector('.closs13');
+
+var popup14=document.querySelector('.popup14');
+var btn14=document.querySelector('.btn14');
+var closs14=document.querySelector('.closs14');
+
+var popup15=document.querySelector('.popup15');
+var btn15=document.querySelector('.btn15');
+var closs15=document.querySelector('.closs15');
+
+var popup21=document.querySelector('.popup21');
+var btn21=document.querySelector('.btn21');
+var closs21=document.querySelector('.closs21');
+
+var popup22=document.querySelector('.popup22');
+var btn22=document.querySelector('.btn22');
+var closs22=document.querySelector('.closs22');
+
+var popup23=document.querySelector('.popup23');
+var btn23=document.querySelector('.btn23');
+var closs23=document.querySelector('.closs23');
+
+var popup24=document.querySelector('.popup24');
+var btn24=document.querySelector('.btn24');
+var closs24=document.querySelector('.closs24');
+
+var popup25=document.querySelector('.popup25');
+var btn25=document.querySelector('.btn25');
+var closs25=document.querySelector('.closs25');
+
+var popup26=document.querySelector('.popup26');
+var btn26=document.querySelector('.btn26');
+var closs26=document.querySelector('.closs26');
+
+var popup27=document.querySelector('.popup27');
+var btn27=document.querySelector('.btn27');
+var closs27=document.querySelector('.closs27');
+
+var popup28=document.querySelector('.popup28');
+var btn28=document.querySelector('.btn28');
+var closs28=document.querySelector('.closs28');
+
+
+btn11.addEventListener('click',()=>{
+popup11.classList.add('active');
+
+})
+closs11.addEventListener('click',()=>{
+popup11.classList.remove('active');
+})
+
+btn12.addEventListener('click',()=>{
+popup12.classList.add('active');
+
+})
+closs12.addEventListener('click',()=>{
+popup12.classList.remove('active');
+})
+
+btn13.addEventListener('click',()=>{
+popup13.classList.add('active');
+
+})
+closs13.addEventListener('click',()=>{
+popup13.classList.remove('active');
+})
+
+btn14.addEventListener('click',()=>{
+popup14.classList.add('active');
+
+})
+closs14.addEventListener('click',()=>{
+popup14.classList.remove('active');
+})
+
+btn15.addEventListener('click',()=>{
+popup15.classList.add('active');
+
+})
+closs15.addEventListener('click',()=>{
+popup15.classList.remove('active');
+})
+
+btn21.addEventListener('click',()=>{
+popup21.classList.add('active');
+
+})
+closs21.addEventListener('click',()=>{
+popup21.classList.remove('active');
+})
+
+btn22.addEventListener('click',()=>{
+popup22.classList.add('active');
+
+})
+closs22.addEventListener('click',()=>{
+popup22.classList.remove('active');
+})
+
+btn23.addEventListener('click',()=>{
+popup23.classList.add('active');
+
+})
+closs23.addEventListener('click',()=>{
+popup23.classList.remove('active');
+})
+btn24.addEventListener('click',()=>{
+popup24.classList.add('active');
+
+})
+closs24.addEventListener('click',()=>{
+popup24.classList.remove('active');
+})
+btn25.addEventListener('click',()=>{
+popup25.classList.add('active');
+
+})
+closs25.addEventListener('click',()=>{
+popup25.classList.remove('active');
+})
+btn26.addEventListener('click',()=>{
+popup26.classList.add('active');
+
+})
+closs26.addEventListener('click',()=>{
+popup26.classList.remove('active');
+})
+btn27.addEventListener('click',()=>{
+popup27.classList.add('active');
+
+})
+closs27.addEventListener('click',()=>{
+popup27.classList.remove('active');
+})
+
+btn28.addEventListener('click',()=>{
+popup28.classList.add('active');
+
+})
+closs28.addEventListener('click',()=>{
+popup28.classList.remove('active');
+})
---
+++ main.js
@@ -0,0 +1,188 @@
+
+// var morecard=document.querySelector('.more-card');
+// var seebtn=document.querySelector('.see-all');
+
+
+
+// seebtn.addEventListener('click',()=>{
+// morecard.classList.toggle('active');
+// })
+
+
+var popup=document.querySelector('.popup');
+var btn1=document.querySelector('.btn1');
+var closs1=document.querySelector('.closs1');
+
+
+var popup2=document.querySelector('.popup2');
+var btn2=document.querySelector('.btn2');
+var closs2=document.querySelector('.closs2');
+
+var popup3=document.querySelector('.popup3');
+var btn3=document.querySelector('.btn3');
+var closs3=document.querySelector('.closs3');
+
+var popup4=document.querySelector('.popup4');
+var btn4=document.querySelector('.btn4');
+var closs4=document.querySelector('.closs4');
+
+var popup5=document.querySelector('.popup5');
+var btn5=document.querySelector('.btn5');
+var closs5=document.querySelector('.closs5');
+
+
+var popup6=document.querySelector('.popup6');
+var btn6=document.querySelector('.btn6');
+var closs6=document.querySelector('.closs6');
+
+var popup7=document.querySelector('.popup7');
+var btn7=document.querySelector('.btn7');
+var closs7=document.querySelector('.closs7');
+
+var popup8=document.querySelector('.popup8');
+var btn8=document.querySelector('.btn8');
+var closs8=document.querySelector('.closs8');
+
+
+var popup9=document.querySelector('.popup9');
+var btn9=document.querySelector('.btn9');
+var closs9=document.querySelector('.closs9');
+
+
+var popup10=document.querySelector('.popup10');
+var btn10=document.querySelector('.btn10');
+var closs10=document.querySelector('.closs10');
+
+// after 10
+
+
+
+var popup16=document.querySelector('.popup16');
+var btn16=document.querySelector('.btn16');
+var closs16=document.querySelector('.closs16');
+
+var popup17=document.querySelector('.popup17');
+var btn17=document.querySelector('.btn17');
+var closs17=document.querySelector('.closs17');
+
+var popup18=document.querySelector('.popup18');
+var btn18=document.querySelector('.btn18');
+var closs18=document.querySelector('.closs18');
+
+var popup19=document.querySelector('.popup19');
+var btn19=document.querySelector('.btn19');
+var closs19=document.querySelector('.closs19');
+
+var popup20=document.querySelector('.popup20');
+var btn20=document.querySelector('.btn20');
+var closs20=document.querySelector('.closs20');
+
+
+var tbg1=document.querySelector('.t-bg1');
+
+var popup29=document.querySelector('.popup29');
+var btn29=document.querySelector('.btn29');
+var closs29=document.querySelector('.closs29');
+
+var popup30=document.querySelector('.popup30');
+var btn30=document.querySelector('.btn30');
+var closs30=document.querySelector('.closs30');
+
+var popup31=document.querySelector('.popup31');
+var btn31=document.querySelector('.btn31');
+var closs31=document.querySelector('.closs31');
+
+
+
+
+
+
+btn1.addEventListener('click',()=>{
+popup.classList.add('active');
+})
+
+closs1.addEventListener('click',()=>{
+popup.classList.remove('active');
+})
+
+
+
+btn2.addEventListener('click',()=>{
+popup2.classList.add('active');
+
+})
+closs2.addEventListener('click',()=>{
+popup2.classList.remove('active');
+})
+
+
+btn3.addEventListener('click',()=>{
+popup3.classList.add('active');
+
+})
+closs3.addEventListener('click',()=>{
+popup3.classList.remove('active');
+})
+
+btn4.addEventListener('click',()=>{
+popup4.classList.add('active');
+
+})
+closs4.addEventListener('click',()=>{
+popup4.classList.remove('active');
+})
+
+
+btn5.addEventListener('click',()=>{
+popup5.classList.add('active');
+
+})
+closs5.addEventListener('click',()=>{
+popup5.classList.remove('active');
+})
+
+
+btn6.addEventListener('click',()=>{
+popup6.classList.add('active');
+
+})
+closs6.addEventListener('click',()=>{
+popup6.classList.remove('active');
+})
+
+btn7.addEventListener('click',()=>{
+popup7.classList.add('active');
+
+})
+closs7.addEventListener('click',()=>{
+popup7.classList.remove('active');
+})
+
+btn8.addEventListener('click',()=>{
+popup8.classList.add('active');
+
+})
+closs8.addEventListener('click',()=>{
+popup8.classList.remove('active');
+})
+
+btn9.addEventListener('click',()=>{
+popup9.classList.add('active');
+
+})
+closs9.addEventListener('click',()=>{
+popup9.classList.remove('active');
+})
+
+btn10.addEventListener('click',()=>{
+popup10.classList.add('active');
+
+})
+closs10.addEventListener('click',()=>{
+popup10.classList.remove('active');
+})
+
+// afrer ten
+
+
+
